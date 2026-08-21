25b8b973f95b1031f3fc888aaa5566baeadb3df0
VasundharaAcharyaNayak <acharv2@rpi.edu>
2026-08-21 13:18:59 -0700

    Add test_data.py for observational data analysis

---
+++ test_data.py
@@ -0,0 +1,30 @@
+from src.config import load_config
+from src.data import (
+    load_observational_data,
+    prepare_pima_cohort,
+)
+
+
+config = load_config("configs/glucose.yaml")
+
+df = load_observational_data(config)
+
+analysis_df = prepare_pima_cohort(
+    df,
+    config,
+)
+
+print("Experiment:", config["experiment"]["id"])
+print("Analytic N:", len(analysis_df))
+
+treatment_col = config["experiment"]["treatment"]["column"]
+
+print(
+    "Eligible T=1:",
+    int(analysis_df[treatment_col].sum()),
+)
+
+print(
+    "Adjustment variables:",
+    config["covariates"]["adjustment"],
+)
