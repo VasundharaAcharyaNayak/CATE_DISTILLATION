616e8e005b84e0624188220974568ab4c2c45d28
VasundharaAcharyaNayak <acharv2@rpi.edu>
2026-08-22 00:35:13 -0700

    Fix parameter name for effect modifier columns

--- experiments/run_smoking.py
+++ experiments/run_smoking.py
@@ -492,8 +492,7 @@ def run_smoking(
             evaluation_df=
                 evaluation_df,
 
-            effect_modifier_columns=
-                effect_modifier_cols,
+            effect_modifier_cols=effect_modifier_columns,
 
             confounder_columns=
                 confounder_cols,
